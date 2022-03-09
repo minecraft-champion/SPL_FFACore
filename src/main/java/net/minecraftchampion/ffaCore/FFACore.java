e26198b8778c5f674bd6c7a18aafd8a336b6d0d7
anhgelus <anhgelus.morhtuuzh@gmx.com>
2022-03-09 18:07:36 +0100

    feat no damage when player is in the spawn

--- src/main/java/net/minecraftchampion/ffaCore/FFACore.java
+++ src/main/java/net/minecraftchampion/ffaCore/FFACore.java
@@ -1,12 +1,14 @@
 package net.minecraftchampion.ffaCore;
 
 import net.minecraftchampion.ffaCore.command.set.SetCommand;
+import net.minecraftchampion.ffaCore.listener.DamagePlayer;
 import net.minecraftchampion.ffaCore.listener.DeathPlayer;
 import net.minecraftchampion.ffaCore.listener.JoinPlayer;
 import net.minecraftchampion.ffaCore.manager.ConfigManager;
 import net.minecraftchampion.ffaCore.manager.FileManager;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
+import org.bukkit.World;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -32,14 +34,18 @@ public final class FFACore extends JavaPlugin {
         final YamlConfiguration kit = configManager.loadKit();
         final PluginManager pluginManager = Bukkit.getPluginManager();
 
-        Bukkit.getWorld("world").setGameRuleValue("naturalRegeneration", "false");
-        Bukkit.getWorld("world").setGameRuleValue("doMobSpawning", "false");
-        Bukkit.getWorld("world").setGameRuleValue("doMobLoot", "false");
-        Bukkit.getWorld("world").setGameRuleValue("doDaylightCycle", "false");
-        Bukkit.getWorld("world").setTime(6000);
+        final World world = Bukkit.getWorld("world");
+
+        world.setGameRuleValue("naturalRegeneration", "false");
+        world.setGameRuleValue("doMobSpawning", "false");
+        world.setGameRuleValue("doMobLoot", "false");
+        world.setGameRuleValue("doDaylightCycle", "false");
+        world.setTime(6000);
+        world.setPVP(true);
 
         pluginManager.registerEvents(new JoinPlayer(kit, config), this);
         pluginManager.registerEvents(new DeathPlayer(kit, config), this);
+        pluginManager.registerEvents(new DamagePlayer(config), this);
 
         getCommand("set").setExecutor(new SetCommand(config, FileManager.getConfig(this)));
     }
--- src/main/java/net/minecraftchampion/ffaCore/listener/DamagePlayer.java
+++ src/main/java/net/minecraftchampion/ffaCore/listener/DamagePlayer.java
@@ -1,2 +1,39 @@
-package net.minecraftchampion.ffaCore.listener;public class DamagePlayer {
+package net.minecraftchampion.ffaCore.listener;
+
+import net.minecraftchampion.ffaCore.manager.ConfigManager;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+public class DamagePlayer implements Listener {
+
+    private final YamlConfiguration config;
+
+    public DamagePlayer(YamlConfiguration config) {
+        this.config = config;
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void EntityDamageEvent(EntityDamageEvent event){
+        final Entity e = event.getEntity();
+        final double y = config.getDouble(ConfigManager.SPAWN + "y");
+
+        if (!(e instanceof Player) || y == 0) {
+            return;
+        }
+
+        final Player player = ((Player) e).getPlayer();
+
+        if (event.getCause() == EntityDamageEvent.DamageCause.FALL){
+            event.setCancelled(true);
+        }
+
+        if (e.getLocation().getBlockY() >= (y - 1)) {
+            event.setCancelled(true);
+        }
+    }
 }
